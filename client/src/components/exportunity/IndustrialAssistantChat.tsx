85617baf8c3d53749f5252d3cd361dc78682ef81
vsounouvou <38376186+vsounouvou@users.noreply.github.com>
2026-08-04 02:36:40 +0000

    Polish industrial chat suggestions

--- client/src/components/exportunity/IndustrialAssistantChat.tsx
+++ client/src/components/exportunity/IndustrialAssistantChat.tsx
@@ -438,7 +438,7 @@ export function IndustrialAssistantChat({
       </div>
 
       {!intake ? (
-        <div className="flex gap-2 overflow-x-auto border-t border-white/10 px-3 py-2 sm:flex-nowrap sm:px-4">
+        <div className="flex gap-2 overflow-x-auto border-t border-white/10 px-3 py-2 [-ms-overflow-style:none] [scrollbar-width:none] [&::-webkit-scrollbar]:hidden sm:flex-nowrap sm:px-4">
           {copy.firstReplies.map((reply) => (
             <button
               key={reply}
